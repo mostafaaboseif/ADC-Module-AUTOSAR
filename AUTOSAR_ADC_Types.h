82dc7402d76cc59b1ee8c358e9119227990dd4ec
Thomas Joseph <1600440@eng.asu.edu.eg>
2020-06-23 00:48:27 +0200

    Delete AUTOSAR_ADC_Types.h

--- AUTOSAR_ADC_Types.h
+++
@@ -1,5 +0,0 @@
-/* Numeric ID of an ADC channel group. */
-typedef uint8_t Adc_GroupType
-/* Type for reading the converted values of a channel group 
-(raw, without further scaling, alignment according precompile switch ADC_RESULT_ALIGNMENT). */
-typedef sint32_t Adc_ValueGroupType
