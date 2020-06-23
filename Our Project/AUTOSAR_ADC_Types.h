521349a41bdbc2a7a90b2162095e297582707a4c
Thomas Joseph <1600440@eng.asu.edu.eg>
2020-06-23 11:48:38 +0200

    Changed the Adc_ValueGroupType from 32 to 16 bits
    The cortex M4-F ADC resolution is 12 bits

--- Our Project/AUTOSAR_ADC_Types.h
+++ Our Project/AUTOSAR_ADC_Types.h
@@ -2,4 +2,4 @@
 typedef uint8_t Adc_GroupType
 /* Type for reading the converted values of a channel group 
 (raw, without further scaling, alignment according precompile switch ADC_RESULT_ALIGNMENT). */
-typedef sint32_t Adc_ValueGroupType
+typedef sint16_t Adc_ValueGroupType
