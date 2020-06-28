799fe42f11a6838db73b272047413ee05657489a
mahmoud <me@mahmoud.com>
2020-06-28 04:56:27 +0200

    added autosar tags

--- ADC Driver (Tiva)/Objects/adc_pbcfg.d
+++
@@ -1,9 +0,0 @@
-.\objects\adc_pbcfg.o: MCAL\Adc Driver\Adc_PBcfg.c
-.\objects\adc_pbcfg.o: MCAL\Adc Driver\Adc.h
-.\objects\adc_pbcfg.o: MCAL\Adc Driver\../Std_Types/Std_Types.h
-.\objects\adc_pbcfg.o: MCAL\Adc Driver\../Std_Types/Compiler.h
-.\objects\adc_pbcfg.o: MCAL\Adc Driver\../Std_Types/Platform_Types.h
-.\objects\adc_pbcfg.o: MCAL\Adc Driver\Adc_TypeDefs.h
-.\objects\adc_pbcfg.o: MCAL\Adc Driver\Adc_Cfg.h
-.\objects\adc_pbcfg.o: MCAL\Adc Driver\M4MemMap.h
-.\objects\adc_pbcfg.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- ADC Driver (Tiva)/Objects/interrupt.d
+++
@@ -1,3 +0,0 @@
-.\objects\interrupt.o: MCAL\interrupt\interrupt.c
-.\objects\interrupt.o: MCAL\interrupt\interrupt.h
-.\objects\interrupt.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
